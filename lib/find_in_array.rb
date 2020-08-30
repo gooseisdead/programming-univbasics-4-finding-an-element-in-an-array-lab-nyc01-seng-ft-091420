2cdfeb2fd85decb4fe42ab46798e743199067243
Mike Smith <gooseisdead@mac.com>
2020-08-30 01:54:35 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
-def find_element_index(array = [2,3,4,5], 2)
+def find_element_index(array = [2,3,4,5], value_to_find)
   count = 0
   while count < array.length do
     puts array[count] == 2
     count += 1
-  end  
+  end
 end
